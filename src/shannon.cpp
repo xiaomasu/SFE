5b0a49329f71ef0d46111fb4267b74bd06dc9e88
zz <3483627576@qq.com>
2025-07-23 18:33:59 +0800

    第一次提交

--- src/shannon.cpp
+++ src/shannon.cpp
@@ -11,26 +11,33 @@ struct ParallelShannon: public Worker{
   RcppParallel::RVector<double> res;
 
   ParallelShannon(const arma::sp_mat& x, Rcpp::NumericVector res)
-      :x(x), res(res){ }
+      :x(x), res(res) { }
   
   void operator()(std::size_t begin, std::size_t end){
     for (uword i = begin; i < end; i++){
       arma::sp_vec column_val = x.col(i);  
       double col_sum = arma::accu(column_val);  
+      uword num_features = column_val.n_nonzero;
 
       if (col_sum <= 0 || column_val.n_nonzero == 0) {  
-          res[i] = 0.0;  
+          res[i,0] = 0.0;
+	  res[i,1] = 0.0;  
           continue;  
       }  
 
-      double tmp_entropy = 0.0;  
+      double shannon = 0.0;  
       
       for (arma::sp_vec::const_iterator it = column_val.begin(); it != column_val.end(); ++it) {  
           double p = *it / col_sum;  
-          tmp_entropy -= p * std::log(p);  
+          shannon -= p * std::log(p);  
       }  
       
-      res[i] = tmp_entropy;  
+      double pielou = 0.0;
+      if(column_val.n_nonzero > 1){
+	      pielou = shannon / (std::log(num_features) / std::log(2.0));
+      }
+      res[i,0] = shannon;
+      res[i,1] = pielou;  
      
     }
   }
@@ -40,13 +47,13 @@ struct ParallelShannon: public Worker{
 // [[Rcpp::export]]
 NumericVector cal_shannon(arma::sp_mat x){
   int n = x.n_cols;
-  Rcpp::NumericVector entropy(n);
-
-  ParallelShannon parallelshannon(x, entropy);
+  NumericMatrix res(n,2);
+  ParallelShannon parallelshannon(x, res);
 
   parallelFor(0, n, parallelshannon);
 
-  return(entropy);
+  return as<NumericVector>(res);
+ 
 }
 
 
